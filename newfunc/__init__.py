15b07cdf83c91f0cc9f50b598635554c93d99b6a
Rai <akhlesh.rai@capgemini.com>
2021-08-09 20:37:12 +0530

    first commit

---
+++ newfunc/__init__.py
@@ -0,0 +1,147 @@
+
+import azure.functions as func
+import requests
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta, TH
+import mimetypes
+import logging
+import matplotlib.pyplot as plt
+import os
+import tempfile
+from azure.storage.blob import BlockBlobService,ContentSettings
+from . import optionlibraries
+from datetime import date
+import jinja2
+
+account_name = 'optiontablestorage'
+account_key = 'eURoD2XMmUgt7zuY9jX9IPXs9WhO5xCkU2du8gcibAdAeGFTmQUKVbyKy+MIN3UQxAu/AV6SMkzmVpimibI2EQ=='
+mycontainer="opt-table"
+
+
+
+def main(req: func.HttpRequest,  context: func.Context) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    block_blob_service = BlockBlobService(
+    account_name=account_name,
+    account_key=account_key
+    )
+
+    str_next_thursday_expiry,_=optionlibraries.nextThu_and_lastThu_expiry_date()
+    pcr,timestamp,niftySpot,df_call_near_expiry,df_put_near_expiry=optionlibraries.optionChain(str_next_thursday_expiry)
+    df_call_near_expiry=df_call_near_expiry.drop(['identifier','pChange','underlying','bidprice','askQty','askPrice','totalSellQuantity','bidQty','change','totalBuyQuantity'], axis = 1)
+    df_call_near_expiry['changeinOpenInterest']=75 * pd.to_numeric(df_call_near_expiry['changeinOpenInterest'])
+    df_call_near_expiry['openInterest'] = 75 * pd.to_numeric(df_call_near_expiry['openInterest'])
+    df_call_near_expiry=df_call_near_expiry.nlargest(5, ['openInterest'])
+    df_put_near_expiry=df_put_near_expiry.drop(['identifier','pChange','underlying','bidprice','askQty','askPrice','totalSellQuantity','bidQty','change','totalBuyQuantity'], axis = 1)
+    df_put_near_expiry['openInterest'] = 75 * pd.to_numeric(df_put_near_expiry['openInterest'])
+    df_put_near_expiry['changeinOpenInterest']=75 * pd.to_numeric(df_put_near_expiry['changeinOpenInterest'])
+    df_put_near_expiry=df_put_near_expiry.nlargest(5, ['openInterest'])
+
+    #market Status
+    marketStatus,marketStatusValue =optionlibraries.marketStatus()
+    #calculate Option Greeks for Call
+    if not (date.today().strftime("%d-%b-%Y")==df_call_near_expiry['expiryDate'].iloc[0]):
+        #calculate Option Greeks for Call only when expiry date is not equal to todays date
+        df_call_near_expiry=optionlibraries.calculateOptionGreeks(df_call_near_expiry,'Call',str_next_thursday_expiry)
+
+        #Calculate Option Greeks for Put
+        df_put_near_expiry=optionlibraries.calculateOptionGreeks(df_put_near_expiry,'Put',str_next_thursday_expiry)
+
+    ## Update the columns for plotting graphs based on OI, Change in OI and Volume
+    df_call_for_graph=df_call_near_expiry.drop(['expiryDate','pchangeinOpenInterest','impliedVolatility','lastPrice'], axis = 1)
+    df_put_for_graph=df_put_near_expiry.drop(['expiryDate','pchangeinOpenInterest','impliedVolatility','lastPrice'], axis = 1)
+    df_call_near_expiry=df_call_near_expiry.drop(['underlyingValue','expiryDate'], axis = 1)
+    df_put_near_expiry=df_put_near_expiry.drop(['underlyingValue','expiryDate'], axis = 1)
+
+    df_put_for_graph=df_put_for_graph.sort_index(axis = 0) 
+    df_call_for_graph=df_call_for_graph.sort_index(axis = 0) 
+
+    ax = df_call_for_graph.plot(x="strikePrice", y=["openInterest","changeinOpenInterest","totalTradedVolume"], kind="bar")
+    ax.get_yaxis().get_major_formatter().set_scientific(False)
+    ax.set_title('Top 5 - Call Option OI, Volume and Changes in OI', fontsize=10)
+    for label in ax.xaxis.get_ticklabels():
+        label.set_rotation(45)    
+    for p in ax.patches:
+        ax.annotate(str(round(p.get_height(),2)), (p.get_x() * 1.005, p.get_height() * 1.005),color='blue')
+    plt.tight_layout()
+    temp_path = tempfile.gettempdir()
+    file_path = os.path.join(temp_path, 'callchartOI.png')
+    
+    plt.savefig(file_path, format = 'png')
+    
+    # saving file to Blob Container
+    block_blob_service.create_blob_from_path(
+    mycontainer,
+    "callchartOI.png",
+    file_path,
+    content_settings=ContentSettings(content_type='image/png')
+    )
+
+    ax = df_put_for_graph.plot(x="strikePrice", y=["openInterest","changeinOpenInterest","totalTradedVolume"], kind="bar")
+    ax.get_yaxis().get_major_formatter().set_scientific(False)
+    ax.set_title('Top 5 - Put Option OI, Volume and Changes in OI', fontsize=10)
+    for label in ax.xaxis.get_ticklabels():
+        label.set_rotation(45)
+    for p in ax.patches:
+        ax.annotate(str(round(p.get_height(),2)), (p.get_x() * 1.005, p.get_height() * 1.005),color='blue')
+    plt.tight_layout()
+
+    file_path = os.path.join(temp_path, 'putchartOI.png')
+    plt.savefig(file_path, format = 'png')
+    block_blob_service.create_blob_from_path(
+    mycontainer,
+    "putchartOI.png",
+    file_path,
+    content_settings=ContentSettings(content_type='image/png')
+    )
+
+    df_global=optionlibraries.global_indices()
+    column_names = ["Current Market parameters","Value"]
+    df_mark_par = pd.DataFrame(columns = column_names)
+    indVix=optionlibraries.indiavix()
+    df_mark_par.loc[len(df_mark_par.index)] = ["India VIX",indVix] 
+    df_mark_par.loc[len(df_mark_par.index)] = ["Put Call Ratio (PCR)",pcr] 
+    df_mark_par= df_mark_par.style.hide_index().applymap(optionlibraries.color_negative_red, subset=['Value']).render()
+
+    if "-" in marketStatusValue:
+        colorvalue = 'red'
+    else:
+        colorvalue = 'green'
+
+    htmltemplate= "<head> <title>Dynamic Option Strategy</title></head>" + "\n" +"<h2> Nifty Spot : <b style=\"color:" + colorvalue+ "\">" + str(niftySpot) + "\t" +"<i>" + str(marketStatusValue) + "</i> </b>" + "| Market Status : " + str(marketStatus) + " | Last Updated Time : " + timestamp + "</h2> <h3>" + df_global + "</h3><h3>" + df_mark_par + "</h3>"
+    tmpvarcall="<h4> Top 5 Strike price of Call Option based on Open Interest for Expiry Date = "+str_next_thursday_expiry + " </h4>"
+    tmpvarput="<h4> Top 5 Strike price of Put  Option based on Open Interest for Expiry Date = "+str_next_thursday_expiry + " </h4>"
+    df_put_near_expiry=df_put_near_expiry.rename({'strikePrice': 'Strike Price', 'expiryDate': 'Option Expiry Date','openInterest':'Open Interest (OI)','changeinOpenInterest':'Change in OI','pchangeinOpenInterest':'% Change in OI','totalTradedVolume':'Traded Volume','impliedVolatility':'IV','lastPrice':'Price'}, axis=1)
+    df_call_near_expiry=df_call_near_expiry.rename({'strikePrice': 'Strike Price', 'expiryDate': 'Option Expiry Date','openInterest':'Open Interest (OI)','changeinOpenInterest':'Change in OI','pchangeinOpenInterest':'% Change in OI','totalTradedVolume':'Traded Volume','impliedVolatility':'IV','lastPrice':'Price'}, axis=1)
+    df_call_near_expiry.set_index('Strike Price', inplace=True)
+    df_put_near_expiry.set_index('Strike Price', inplace=True)
+
+
+    df_call_near_expiry=df_call_near_expiry.reset_index(drop=False)
+    iddx1 = df_call_near_expiry['Strike Price'].sub(niftySpot).abs().idxmin()
+    idx1 = pd.IndexSlice
+    slice1_ = idx1[idx1[iddx1]]
+    call_render=df_call_near_expiry.style.hide_index().format(formatter={('Price'): "{:.2f}",('Delta(Δ)'): "{:.3f}",('Gamma(γ)'): "{:.3f}",('Theta(Θ)'): "{:.3f}",('% Change in OI'): "{:.2f}",('IV'): "{:.2f}"}).set_properties(**{'background-color': '#ffffb3'},**{'width': '100'}, subset=slice1_).render()
+
+
+
+    df_put_near_expiry=df_put_near_expiry.reset_index(drop=False)
+    iddx2 = df_put_near_expiry['Strike Price'].sub(niftySpot).abs().idxmin()
+    idx2 = pd.IndexSlice
+    slice2_ = idx2[idx2[iddx2]]
+    put_render=df_put_near_expiry.style.hide_index().format(formatter={('Price'): "{:.2f}",('Delta(Δ)'): "{:.3f}",('Gamma(γ)'): "{:.3f}",('Theta(Θ)'): "{:.3f}",('% Change in OI'): "{:.2f}",('IV'): "{:.2f}"}).set_properties(**{'background-color': '#ffffb3'},**{'width': '100px'}, subset=slice2_).render() 
+
+     
+    filename = os.path.join(temp_path, 'index.html')
+    tobePrinted= htmltemplate + "<h3> Option Chain data for " + str_next_thursday_expiry + " </h3>"+ "\n" +"\n" + tmpvarcall + "\n" + "\n" + call_render   + "<h5>" + "<img src='https://optiontablestorage.blob.core.windows.net/opt-table/callchartOI.png'>"+ "</h5>" + "\n===========================================================" + "\n\n" + "\n" +"\n" + tmpvarput + "\n" + "\n" + put_render + "\n" + "<h5>" + "<img src='https://optiontablestorage.blob.core.windows.net/opt-table/putchartOI.png'>"+ "</h5>" + "==========================================================="
+   
+    
+    text_file = open(filename, "w", encoding='utf8')
+    text_file.write(tobePrinted)
+    text_file.close()
+
+    #filename = f"/tmp/index.html"
+    with open(filename, 'rb') as f:
+        mimetype = mimetypes.guess_type(filename)
+        return func.HttpResponse(f.read(), mimetype=mimetype[0])
---
+++ newfunc/optionlibraries.py
@@ -0,0 +1,242 @@
+
+import requests
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta, TH
+import quantsbin.derivativepricing as qbdp
+from quantsbin.derivativepricing.namesnmapper import VanillaOptionType, ExpiryType, UdlType, OBJECT_MODEL, DerivativeType
+from datetime import date
+from lxml import html
+from lxml.etree import tostring
+
+DIV_YIELD= 0.0344 # RBI Dividend yield
+def marketStatus():
+    url="https://www.nseindia.com/api/marketStatus"
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "authority": "www.nseindia.com",
+      "scheme":"https"
+    }
+    res = requests.get(url, headers=urlheader).json()
+    mstatus=res['marketState'][0]['marketStatus']
+
+    url="https://www.nseindia.com"
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "authority": "www.nseindia.com",
+      "scheme":"https"
+    }
+    res = requests.get(url, headers=urlheader)
+    tree = html.fromstring(res.content)  
+    marketChange = tree.xpath('//*[@id="marketStat0"]/div[2]/span/text()')
+    return(mstatus,marketChange[0])
+
+def optionChain (expirydate):
+
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "authority": "www.nseindia.com",
+      "scheme":"https"
+    }
+        
+    url="https://www.nseindia.com/api/option-chain-indices?"
+    params="symbol=NIFTY"
+    
+    url_encoded=url + params
+    
+    req = requests.get(url_encoded, headers=urlheader).json()
+    call = []
+    put = []
+    strikePrices=[]
+    timestamp=req['records']['timestamp']
+    niftySpot=req['records']['underlyingValue']
+    for eachstrike in req['records']['strikePrices']:
+        strikePrices.append(eachstrike)
+    for each in strikePrices:
+        for rec in req['records']['data']:
+            if rec['expiryDate']==expirydate and rec['strikePrice']==each:
+                temp_call={}
+                temp_put={}
+                for k,v in rec['CE'].items():
+                     temp_call[k] = v
+                call.append(temp_call)
+                for k,v in rec['PE'].items():
+                    temp_put[k]=v
+                put.append(temp_put)
+                
+                
+    df_call = pd.DataFrame(call)
+    df_put = pd.DataFrame(put)
+        
+
+    totCallOI=req['filtered']['CE']['totOI']
+    totputOI=req['filtered']['PE']['totOI']
+    pcr="{:.2f}".format(totputOI/totCallOI)
+        
+
+    return(pcr,timestamp,niftySpot,df_call,df_put)
+
+
+
+def nextThu_and_lastThu_expiry_date ():
+
+    todayte = datetime.today()
+    
+    cmon = todayte.month
+    if_month_next=(todayte + relativedelta(weekday=TH(1))).month
+    next_thursday_expiry=todayte + relativedelta(weekday=TH(1))
+   
+    if (if_month_next!=cmon):
+        month_last_thu_expiry= todayte + relativedelta(weekday=TH(5))
+        if (month_last_thu_expiry.month!=if_month_next):
+            month_last_thu_expiry= todayte + relativedelta(weekday=TH(4))
+    else:
+        for i in range(1, 7):
+            t = todayte + relativedelta(weekday=TH(i))
+            if t.month != cmon:
+                # since t is exceeded we need last one  which we can get by subtracting -2 since it is already a Thursday.
+                t = t + relativedelta(weekday=TH(-2))
+                month_last_thu_expiry=t
+                break
+    str_month_last_thu_expiry=str(month_last_thu_expiry.strftime("%d")) + "-" + month_last_thu_expiry.strftime("%b").title() + "-" +month_last_thu_expiry.strftime("%Y")
+    str_next_thursday_expiry=str(next_thursday_expiry.strftime("%d")) + "-" + next_thursday_expiry.strftime("%b").title() + "-" + next_thursday_expiry.strftime("%Y")
+    return (str_next_thursday_expiry,str_month_last_thu_expiry)
+
+
+def calculateOptionGreeks (df,option_type,expiryDate):
+    df['Delta(\u0394)']=pd.Series(dtype='float64')
+    df['Gamma(\u03B3)']=pd.Series(dtype='float64')
+    df['Theta(\u0398)']=pd.Series(dtype='float64')
+    pricing_date=date.today().strftime("%Y%m%d")
+    df['expiryDate']= pd.to_datetime(df['expiryDate'])
+    for index,val in df.iterrows():
+        custdate=val['expiryDate'].strftime("%Y%m%d")
+        
+        market1_parameters = {'spot0': float(val['underlyingValue'])
+                     , 'pricing_date':pricing_date
+                     , 'volatility':0.01*float(val['impliedVolatility'])
+                     , 'rf_rate':DIV_YIELD
+                     , 'yield_div':0.0}
+
+        equity_p1 = qbdp.EqOption(option_type=option_type, strike=float(val['strikePrice']), expiry_date=custdate, expiry_type='European')
+
+        eq1_BSM_market1 = equity_p1.engine(model="BSM", **market1_parameters)
+        
+        df.at[index,'Delta(\u0394)']=float(eq1_BSM_market1.risk_parameters()['delta'])
+        df.at[index,'Gamma(\u03B3)']=float(eq1_BSM_market1.risk_parameters()['gamma'])
+        df.at[index,'Theta(\u0398)']=float(eq1_BSM_market1.risk_parameters()['theta'])
+    return df
+
+
+def dowfuture():
+    url="https://www.investing.com/indices/us-30-futures"
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "authority": "www.investing.com",
+      "scheme":"https"
+    }
+    res = requests.get(url, headers=urlheader)
+    tree = html.fromstring(res.content)  
+  
+    # Get element using XPath
+    dow_fut = tree.xpath('//span[@id="last_last"]/text()')
+    dow_fut_change=tree.xpath('//*[@id="quotes_summary_current_data"]/div[1]/div[2]/div[1]/span[2]/text()')
+    dow_fut_perc_change=tree.xpath('//*[@id="quotes_summary_current_data"]/div[1]/div[2]/div[1]/span[4]/text()')
+    
+    return(dow_fut[0],dow_fut_change[0],dow_fut_perc_change[0])
+
+def nikkeifuture():
+    url="https://www.investing.com/indices/japan-225-futures"
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "authority": "www.investing.com",
+      "scheme":"https"
+    }
+    res = requests.get(url, headers=urlheader)
+    tree = html.fromstring(res.content)  
+  
+    # Get element using XPath
+    nik_fut = tree.xpath('//span[@id="last_last"]/text()')
+    nik_fut_change=tree.xpath('//*[@id="quotes_summary_current_data"]/div[1]/div[2]/div[1]/span[2]/text()')
+    nik_fut_perc_change=tree.xpath('//*[@id="quotes_summary_current_data"]/div[1]/div[2]/div[1]/span[4]/text()')
+    return(nik_fut[0],nik_fut_change[0],nik_fut_perc_change[0])
+
+def ftseFuture():
+    url="https://www.investing.com/indices/uk-100-futures"
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "authority": "www.investing.com",
+      "scheme":"https"
+    }
+    res = requests.get(url, headers=urlheader)
+    tree = html.fromstring(res.content)  
+  
+    # Get element using XPath
+    ftse_fut = tree.xpath('//span[@id="last_last"]/text()')
+    ftse_fut_change=tree.xpath('//*[@id="quotes_summary_current_data"]/div[1]/div[2]/div[1]/span[2]/text()')
+    ftse_fut_perc_change=tree.xpath('//*[@id="quotes_summary_current_data"]/div[1]/div[2]/div[1]/span[4]/text()')
+    return(ftse_fut[0],ftse_fut_change[0],ftse_fut_perc_change[0])
+
+
+def indiavix():
+    url="https://www1.nseindia.com/live_market/dynaContent/live_watch/VixDetails.json"
+    urlheader = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "Host": "www1.nseindia.com"
+    }
+    res = requests.get(url,headers=urlheader).json()
+    viX_perc_change=res['currentVixSnapShot'][0]['PERC_CHANGE']
+    vix_current=res['currentVixSnapShot'][0]['CURRENT_PRICE']
+    vix=vix_current + " (" + viX_perc_change + "% )"
+
+    return(vix)
+
+def global_indices():      
+    
+    column_names = ["Global Indices","Current Value"]
+    df_global = pd.DataFrame(columns = column_names)
+     
+    curDow,_,perDowChange=dowfuture()
+    curDow=str(curDow)
+    perDowChange=str(perDowChange)
+    
+    curDow= curDow + "  (" + perDowChange + ")"
+    
+    curFtse,_,perFtseChange=ftseFuture()
+    curFtse=str(curFtse)
+    perFtseChange=str(perFtseChange)
+    
+    curFtse=curFtse + "  (" + perFtseChange + ")"
+    
+    curNik,_,perNikChange=nikkeifuture()
+    curNik=str(curNik)
+    perNikChange=str(perNikChange)
+    
+    curNik=curNik + "  (" + perNikChange + ")"
+    
+    
+    
+    df_global.loc[len(df_global.index)] = ["Dow Futures (US)",curDow] 
+    df_global.loc[len(df_global.index)] = ["Nikkei Futures (Japan)",curNik] 
+    df_global.loc[len(df_global.index)] = ["FTSE Futures (London)",curFtse] 
+    df_global_render= df_global.style.hide_index().applymap(color_negative_red, subset=['Current Value']).render()
+    
+    return(df_global_render)
+
+
+def color_negative_red(value):
+  """
+  Colors elements in a dateframe
+  green if positive and red if
+  negative. Does not color NaN
+  values.
+  """
+
+  if "-" in value:
+    color = 'red'
+  elif "+" in value:
+    color = 'green'
+  else:
+    color = 'black'
+
+  return 'color: %s' % color
